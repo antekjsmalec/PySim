ba09c564f7feaf33e8e17f34c8882aee650b37a7
antekjsmalec <antekj.smalec@gmail.com>
2026-07-31 23:36:37 +0100

    Started from scrath tbh, old code is still accessible

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.Clock()
+pygame.display.set_caption("PySim")
 run = True
 dt = 0
-mousePositionX, mousePositionY = pygame.mouse.get_pos()
-physicsOn = False
-g = 0.981
+physics_on = False
 
 class Button:
     def __init__(self, posX, posY, height, width, colour):
@@ -22,76 +21,71 @@ class Button:
 
         self.rect = pygame.Rect(posX, posY, height, width)
 
-    def touchingMouse(self):
+    def touching_mouse(self):
         if mousePositionX >= self.posX and mousePositionX <= (self.posX+self.width) and mousePositionY >= self.posY and mousePositionY <= (self.posY + self.height):
             return True
 
+class Vector:
 
-playPause = Button(10, 10, 25, 25, 'red')
+    def __init__(self, x = 0.0, y = 0.0):
+        self.x = float(x)
+        self.y = float(y)
 
-class Block:
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+    
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
 
-    allBlocks = []
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
 
-    def __init__(self, posX, posY, height, width, startAngle):
-        self.posX = posX
-        self.posY = posY
-        self.height = height
-        self.width = width
-        self.r = math.sqrt(pow(width, 2) + pow(height, 2)) / 2
-        self.v = 0
-        self.freeFall = True
+    def __rmul__(self, scalar):
+        return self.__mul__(scalar)
+    
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
 
-        self.angle = startAngle
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
 
-        self.baseAngA = math.radians(315)
-        self.baseAngB = math.radians(45)
-        self.baseAngC = math.radians(225)
-        self.baseAngD = math.radians(135)
+    def cross(self, other):
+        return self.x * other.y - self.y * other.x
 
-        self.pointA = 0,0
-        self.pointB = 0,0
-        self.pointC = 0,0
-        self.pointD = 0,0
-        
+    def magnitude(self):
+        return math.sqrt(self.x**2 + self.y**2)
 
-        Block.allBlocks.append(self)
+class Particle:
 
+    def __init__(self, x,y, radius=10, mass=1.0, bounce = 0.8):
+        self.position = Vector(x, y)
+        self.v = Vector(0,0)
+        self.a = Vector(0,0)
+        self.radius = radius
+        self.mass = mass
+        self.bounce = bounce
 
-    def update_points(self):
-        radAngle = math.radians(self.angle)
-        
-        angA = self.baseAngA + radAngle
-        angB = self.baseAngB + radAngle
-        angC = self.baseAngC + radAngle
-        angD = self.baseAngD + radAngle
+    def force(self, force:Vector):
+        self.a += force * (1.0 / self.mass)
 
-        self.pointA = (self.posX + self.r * math.cos(angA), self.posY + self.r * math.sin(angA))
-        self.pointB = (self.posX + self.r * math.cos(angB), self.posY + self.r * math.sin(angB))
-        self.pointC = (self.posX + self.r * math.cos(angC), self.posY + self.r * math.sin(angC))
-        self.pointD = (self.posX + self.r * math.cos(angD), self.posY + self.r * math.sin(angD))
+    def update(self, dt = float, floor = float):
+        self.v += self.a * dt
+        self.position += self.v * dt
+        self.a = Vector(0,0)
 
-    def __repr__(self):
-        return f"Block({self.pointA}, {self.pointB}, {self.pointC}, {self.pointD})"
-
-    def update_gravity(self):
-        self.v = self.v + g
-        self.posY =+ self.v
+        if self.position.y + self.radius >= floor:
+            self.position.y = floor - self.radius
+            self.v = -self.v * self.bounce
 
 
-    def rotate(self, degreesDelta):
-        self.angle += degreesDelta
-        self.update_points()
+mousePositionX, mousePositionY = pygame.mouse.get_pos()
 
-    def draw(self):
-        points = [self.pointA, self.pointB, self.pointD, self.pointC, self.pointA, self.pointD]
-        pygame.draw.aalines(screen, 'white', False, points, 2)
+g = Vector(0, 981.0)
+floor__height = 1030
+particle = Particle(600, 50, 10,1, 0.8)
 
-size = 75
-square = Block(250, 250, size, size, 0)
-floor = Block(200, 950, 150, 150, 0)
+play_pause = Button(10, 10, 25, 25, 'red')
 
-rotationSpeed = 90
 
 
 while run:
@@ -99,28 +93,27 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    dt = clock.tick(60) / 1000
     screen.fill((40, 40, 40))
     mousePositionX, mousePositionY = pygame.mouse.get_pos()
 
 
-    pygame.draw.rect(screen, playPause.colour, playPause.rect)
-    if playPause.touchingMouse() == True and pygame.mouse.get_just_released()[0] == True:
-        if playPause.colour == 'red':
-            playPause.colour = 'green'
-            physicsOn = True
-        elif playPause.colour == 'green':
-            playPause.colour = 'red'
-            physicsOn = False
+    pygame.draw.rect(screen, play_pause.colour, play_pause.rect)
+    if play_pause.touching_mouse() == True and pygame.mouse.get_just_released()[0] == True:
+        if play_pause.colour == 'red':
+            play_pause.colour = 'green'
+            physics_on = True
+        elif play_pause.colour == 'green':
+            play_pause.colour = 'red'
+            physics_on = False
 
-    square.update_points()
-    square.draw()
-    square.rotate(rotationSpeed*dt)
-    print(f"Point A:{Block.allBlocks[0].pointA}| Point B: {Block.allBlocks[0].pointB}| Point C:{Block.allBlocks[0].pointC}| Point D: {Block.allBlocks[0].pointD}")
+    if physics_on == True:
+        particle.force(g)
+        particle.update(dt, floor__height)
 
-    floor.update_points()
-    floor.draw()
-    
+    pygame.draw.circle(screen, 'red', (int(particle.position.x), int(particle.position.y)), particle.radius)
+    pygame.draw.line(screen, 'white', (0, floor__height), (1920, floor__height), width=1)
+
     pygame.display.flip()
-    dt = clock.tick(60) / 1000
 
 pygame.quit()
